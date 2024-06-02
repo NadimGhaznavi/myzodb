73323850e07807cfa835c69b8b8ffd07bddc5b23
NadimGhaznavi <nghaznavi@gmail.com>
2024-06-02 05:42:21 -0400

    update

---
+++ assets/py/MyZODBRoot/MyZODBRoot.py
@@ -0,0 +1,52 @@
+class MyZODBRoot:
+    """
+    Abstract root class for the ZenoDB project, inheriting from ZODB.
+
+    Attributes:
+    - _root: The root object of the ZODB database.
+    - _name: The name of the object.
+
+    """
+def __init__(self, root, name):
+        self._root = root
+        self._name = name
+
+    def get_root(self):
+        """
+        Returns the root object of the ZODB database.
+
+        Returns:
+        The root object of the ZODB database.
+
+        """
+return self._root
+
+    def set_root(self, root):
+        """
+        Sets the root object of the ZODB database.
+
+        Args:
+        root: The new root object for the ZODB database.
+
+        """
+self._root = root
+
+    def get_name(self):
+        """
+        Returns the name of the object.
+
+        Returns:
+        The name of the object.
+
+        """
+return self._name
+
+    def __str__(self):
+        """
+        Returns a string representation of the object.
+
+        Returns:
+        A string containing the object's class and name.
+
+        """
+return f"{self.__class__.__name__}(name={self._name})"
