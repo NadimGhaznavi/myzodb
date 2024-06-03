c65023852adbb02294d9db849aad51af8bda6c54
NadimGhaznavi <nghaznavi@gmail.com>
2024-06-03 05:38:13 -0400

    update

--- assets/py/MyZODBRoot/MyZODBRoot.py
+++ assets/py/MyZODBRoot/MyZODBRoot.py
@@ -9,44 +9,33 @@ class MyZODBRoot:
     """
 def __init__(self, root, name):
         self._root = root
-        self._name = name
 
     def get_root(self):
-        """
-        Returns the root object of the ZODB database.
+      """
+      Returns the root object of the ZODB database.
 
-        Returns:
-        The root object of the ZODB database.
+      Returns:
+      * The root object of the ZODB database.
 
-        """
-return self._root
+      """
+      return self._root
 
     def set_root(self, root):
-        """
-        Sets the root object of the ZODB database.
+      """
+      Sets the root object of the ZODB database.
 
-        Args:
-        root: The new root object for the ZODB database.
+      Args:
+      * root: The new root object for the ZODB database.
 
-        """
-self._root = root
-
-    def get_name(self):
-        """
-        Returns the name of the object.
-
-        Returns:
-        The name of the object.
-
-        """
-return self._name
+      """
+      self._root = root
 
     def __str__(self):
-        """
-        Returns a string representation of the object.
+      """
+      Returns a string representation of the object.
 
-        Returns:
-        A string containing the object's class and name.
+      Returns:
+      * A string containing the object's class and name.
 
-        """
-return f"{self.__class__.__name__}(name={self._name})"
+      """
+      return f"{self.__class__.__name__}(name={self._name})"
